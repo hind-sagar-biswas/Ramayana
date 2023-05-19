cfe51133fe4c9c8f3ed1d56ea51e9442a7964761
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-05-19 16:03:44 +0600

    added installation option

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ import subprocess
 
 DEBUG = 0
 
+command = ["python", "-u", "main.py"]
+install = input("$_INSTALL [y/N] ?\n>")
 loc = "src"
-if DEBUG: loc = "dev"
 
-os.chdir(f".\{loc}")
-subprocess.run(["python", "-u", "main.py"], shell=True)
+
+if DEBUG: loc = "dev"
+if install.lower == "y":
+    command = ["pyinstaller"]
+    if not DEBUG: command.append("--noconsole")
+    command.append(f"--icon=./{loc}/images/logo.ico")
+    command.append(f"--add-data=./{loc}/images;images")
+    command.append(f"--add-data=./{loc}/fonts;font")
+    command.append(f"--add-data=./{loc}/book;book")
+    command.append(f"./{loc}/main.py")
+else:
+    os.chdir(f".\{loc}")
+
+subprocess.run(command, shell=True)
